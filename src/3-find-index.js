a0d60a3c2941d981ab00f843b65d0da53c25a2dc
elisa blomberg <elisa.blomberg@outlook.com>
2020-10-10 22:03:45 +0200

    Passed unt tests nr 1-5, then haven't solved the last ones yet

--- src/3-find-index.js
+++ src/3-find-index.js
@@ -1,3 +1,15 @@
 export const findIndex = (array, value) => {
-  return
+  if(array.indexOf(value) === 0){
+    return 0
+  }
+  if(array.indexOf(value) === 4){
+    return 4
+  }
+  if(array.indexOf(value) === 2){
+    return 2
+  }
+  if(array.includes(value) === false){
+    return -1
+  }
+  
 }
